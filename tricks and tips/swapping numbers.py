9dce68961692bb69d5c61c9aed0b0d450e5d0b05
SHANMUKH-CH <shanmukhrodofatos@gmail.com>
2020-11-23 19:24:35 +0530

    trick to swap numbers in python

---
+++ tricks and tips/swapping numbers.py
@@ -0,0 +1,15 @@
+#swapping is an important concept when it comes to data strctures & algorithms
+#normal way to swap
+a=5
+b=6
+temporary_varible=a
+a=b
+b=temporary_varible
+print(f'the value of a after swapping is {a}')
+print(f'the value of b after swapping is {b}')
+#python trick to swap
+a=5
+b=6
+a,b=b,a
+print(f'the value of a after swapping is {a}')
+print(f'the value of b after swapping is {b}')
