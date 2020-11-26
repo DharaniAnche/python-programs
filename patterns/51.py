d94fcbbdc3dcdfdf93da0e2d8cb1be00d7b05c9b
SHANMUKH-CH <shanmukhrodofatos@gmail.com>
2020-11-26 15:31:05 +0530

    pattern

---
+++ patterns/51.py
@@ -0,0 +1,9 @@
+n=int(input('enter a num : '))
+inc=7
+for i in range(n,0,-1):
+    for j in range(i,n):
+        print(' ',end=' ')
+    for k in range(1,inc+1):
+        print(chr(64+i),end=' ')
+    inc-=2
+    print()
