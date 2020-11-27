33cfefa7919205d3789329a2624642c7e6bb51c9
SHANMUKH69 <37172194+SHANMUKH69@users.noreply.github.com>
2020-11-28 03:41:06 +0530

    string rev

---
+++ tricks and tips/reversing a string.py
@@ -0,0 +1,9 @@
+# Python code to reverse a string 
+#normal way - we can use loops; stacks; recursion
+#using slicing methods
+if __name__=='__main__':
+    word='SHANMUKH'
+    #1st method
+    print(word[::-1])
+    #2nd method
+    print(''.join(reversed((word))))
