f4b8afe1a6c9cafe774bfc5f2d7f05a0895b3c18
SHANMUKH69 <37172194+SHANMUKH69@users.noreply.github.com>
2020-12-04 04:23:48 +0530

    printing with index

---
+++ tricks and tips/enumeration.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+names=['shan','sai','osiris','false promise']
+'''
+index=0
+for n in names:
+    print(index, n)
+    index += 1
+'''
+for index, name in enumerate(names):
+    print(index, name)
