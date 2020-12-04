4684585cc5120781a754b9836a94857201b1b7e2
HEIS3NBE3G <roguedante@protonmail.com>
2020-12-04 20:25:12 +0530

    factorial using reduce()

---
+++ tricks and tips/fact_reduce.py
@@ -0,0 +1,7 @@
+#!/bin/bash
+#----printing factorial of a number----
+#reduce is in functools module
+#using reduce(fun, seq)
+import functools  
+n = 5
+print(functools.reduce(lambda x, y: x * y, range(1, n+1)))
